e759566d4a7bf447fb2d802c97458b15502f0409
Aryeh Katz <31622929+katzio@users.noreply.github.com>
2022-11-28 22:34:21 +0200

    add app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { DemoModule } from './demo/demo.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(DemoModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
